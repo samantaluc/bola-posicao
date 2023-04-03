fdb49f9003b837534caa63809cd7cd1b88fce868
Samanta Adriane Sedlaczek Luchoski <49082205+samantaluc@users.noreply.github.com>
2023-04-03 18:25:49 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,14 @@
 var Ball, database;
 var position;
-
 function setup() {
     database = firebase.database();
     console.log(database);
     createCanvas(500, 500);
-    Ball = createSprite(250, 250, 10, 10);
-    Ball.shapeColor = "red";
-    var BallPosition = database.ref("bola/posicao");
-    BallPosition.on("value", readPosition, showError);
+    Ball = createSprite(250, 250, 10, 10); //cria o sprite da bola
+    Ball.shapeColor = "red"; //dá cor a bola
+    var BallPosition = database.ref("bola/posicao"); //pega a referencia do nó posição do banco de dados
+    BallPosition.on("value", readPosition, showError); //lê o valor que está em x e y
 }
-
 function draw() {
     background("white");
     if (keyDown(LEFT_ARROW)) {
@@ -21,24 +19,17 @@ function draw() {
         writePosition(0, -1);
     } else if (keyDown(DOWN_ARROW)) {
         writePosition(0, +1);
-    }
-    drawSprites();
+    }    drawSprites();
 }
-
-function writePosition(x, y) {
-    database.ref("bola/posicao").set({
-        x: position.x + x,
-        y: position.y + y,
-    });
-}
-
-function readPosition(data) {
+function writePosition(x, y) { //escreve a posição do x e y conforme movimentado
+    database.ref("bola/posicao").set({ // set = troca
+        x: position.x + x,        y: position.y + y,
+    });             }
+function readPosition(data) { // lê a posição do banco de dados 
     position = data.val();
-    console.log(position.x);
     Ball.x = position.x;
     Ball.y = position.y;
 }
-
 function showError() {
     console.log("Dados não recebidos do banco de dados");
 }
